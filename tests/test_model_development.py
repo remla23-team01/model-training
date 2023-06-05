8f3eaab31f477eeeaeaa628e8fe21f0b4664ca44
Irtaza Hashmi <i.hashmi@student.tudelft.nl>
2023-06-05 11:53:52 +0300

    Implemented model dev and monitoring tests.

--- tests/test_model_development.py
+++ tests/test_model_development.py
@@ -2,14 +2,31 @@ import os
 
 import joblib
 import numpy as np
+import pytest
 from sklearn.model_selection import train_test_split
 
 from src.preprocess import main
 from src.train_model import train_model
 
 
+@pytest.fixture
+def X():
+    """
+    Load X data
+    """
+    return np.load("data/X.npy")
+
+
+@pytest.fixture
+def y():
+    """
+    Load y data
+    """
+    return np.load("data/y.npy")
+
+
 class TestModelDevelopment:
-    def test_model_development(self):
+    def test_model_development(self, X, y):
         """
         Tests the model performs better than randomly selecting positive or negative.
         """
@@ -17,19 +34,17 @@ class TestModelDevelopment:
             "data/y.npy"
         ):
             main()
-        X, y = np.load("data/X.npy"), np.load("data/y.npy")
         _, X_test, _, y_test = train_test_split(
             X, y, test_size=0.20, random_state=0
         )
         model = joblib.load("ml_models/c2_Classifier_Sentiment_Model")
         assert sum(model.predict(X_test) == y_test) / len(y_test) > 0.5
 
-    def test_non_determinism_robustness(self):
+    def test_non_determinism_robustness(self, X, y):
         """
         Test non-determinism robustness.
         """
         accuracy = 0.9
-        X, y = np.load("data/X.npy"), np.load("data/y.npy")
 
         for i in range(10):
             model = train_model(X, y, random_state=i)
@@ -37,4 +52,22 @@ class TestModelDevelopment:
                 X, y, test_size=0.20, random_state=0
             )
             new_accuracy = sum(model.predict(X_test) == y_test) / len(y_test)
-            assert accuracy - new_accuracy < 0.2
+            assert abs(accuracy - new_accuracy) < 0.2
+
+    def test_data_slices(self, X, y):
+        """
+        Test data slices.
+        """
+
+        model = joblib.load("ml_models/c2_Classifier_Sentiment_Model")
+
+        # data splice
+
+        pos = [X[i] for i in range(len(y)) if y[i]]
+        neg = [X[i] for i in range(len(y)) if not y[i]]
+
+        pos_acc = sum(model.predict(pos) == 1) / len(pos)
+        neg_acc = sum(model.predict(neg) == 0) / len(neg)
+
+        # compare accuracies
+        assert abs(pos_acc - neg_acc) < 0.2
--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -1,3 +1,44 @@
+import numpy as np
+import pytest
+from sklearn.model_selection import train_test_split
+
+from src.train_model import train_model
+
+
+@pytest.fixture
+def X():
+    """
+    Load X data
+    """
+    return np.load("data/X.npy")
+
+
+@pytest.fixture
+def y():
+    """
+    Load y data
+    """
+    return np.load("data/y.npy")
+
+
 class TestMonitoring:
-    def test_monitoring(self):
-        pass
+    def test_model_staleness(self, X, y):
+        """Test for model statelness."""
+
+        # e.g. old data
+        X_old, y_old = X[:300], y[:300]
+        model_old = train_model(X_old, y_old)
+
+        _, X_test, _, y_test = train_test_split(
+            X, y, test_size=0.20, random_state=0
+        )
+
+        old_acc = sum(model_old.predict(X_test) == y_test) / len(y_test)
+
+        # e.g. new data arriving
+        X_new, y_new = X[:800], y[:800]
+        model_new = train_model(X_new, y_new)
+
+        new_acc = sum(model_new.predict(X_test) == y_test) / len(y_test)
+
+        assert new_acc > old_acc
