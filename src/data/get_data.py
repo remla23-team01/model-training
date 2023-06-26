d0c68dba8e7168b2c2656333e076a61f7d3466c7
levijn <levijndejager@gmail.com>
2023-06-26 13:24:16 +0200

    now gets data from google drive

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -1,6 +1,10 @@
 """Gets raw data from a url and saves it to a specified path."""
 import pandas as pd
+import gdown
 
+
+def download_data_gdrive(url, output_path):
+    gdown.download(url, output_path, quiet=False,fuzzy=True)
 
 def download_data(url):
     """
@@ -22,18 +26,16 @@ def save_data(data, path):
         path: str
     """
     print("Saving data...")
-    data.to_csv(path, index=False)
+    data.to_csv(path, sep=",", index=False)
     print(f"Succesfully saved data to {path}")
 
 
 def get_data():
     """Main function to run script"""
-    url = "https://raw.githubusercontent.com/remla23-team01/"
-    url = (
-       url + "model-training/main/data/a1_RestaurantReviews_HistoricDump.tsv"
-    )
-    # url = "data/a1_RestaurantReviews_HistoricDump.tsv"
-    data = download_data(url)
+    url = "https://drive.google.com/file/d/14BaUbCczMHAK5DLjOuLRlpkbIuC4lCSM/view?usp=sharing"
+    output_path = "data/interim/a1_RestaurantReviews_HistoricDump.csv"
+    download_data_gdrive(url, output_path)
+    data = pd.read_csv(output_path, sep=",")
     print(f"Preview of data:\n, {data.head()}")
     path = "data/raw/a1_RestaurantReviews_HistoricDump.csv"
     save_data(data, path)
